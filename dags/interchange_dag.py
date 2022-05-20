e3607fbd15d9eed3b02c3ab67474bb394ee4ca7d
Vadrama Ndisang <vadramandisang@Vadramas-MacBook-Pro.local>
2022-05-20 19:32:38 +0200

    .

--- dags/interchange_dag.py
+++ dags/interchange_dag.py
@@ -13,12 +13,41 @@ catchup=False # Catchup avoids triggering none-trggered DAGs between the start d
 ) as dag:
     run_interchage_container = BashOperator(
         task_id = "run_interchage_container",
-        bash_command = "docker run -ti interchange-python-docker"
+        bash_command = "docker run -ti interchange-python-docker "
     )
 
 
 
-runn_command = "./home/talend/talend_share/python_scripts/Interchange_app/run_interchange.sh "
+
+try:
+   runn_command = "docker run -ti interchange-python-docker "
+   t1 = BashOperator(
+        task_id= 'run_cmd',
+        bash_command=runn_command,
+        dag=dag
+   )
+except:
+  raise Exception("Cannot execute {}".format(runn_command))
+
+with DAG("interchange_dag_1",  # Dag id
+start_date=datetime(2022, 1, 1,0,0,0), # start date, the 1st of January 2022 --> This DAG will starts triggered on 02/01/2022
+schedule_interval="@daily",  # Cron expression, here it is a preset of Airflow, @daily means once every day at midnight.
+catchup=False # Catchup avoids triggering none-trggered DAGs between the start date and the current date
+) as dag1:
+    try:
+        runn_command = "docker run -ti interchange-python-docker "
+        t1 = BashOperator(
+            task_id= 'create_file',
+            bash_command=runn_command,
+            dag=dag1
+        )
+    except:
+        raise Exception("Cannot execute {}".format(runn_command))
+
+"""
+
+#runn_command = "./home/talend/talend_share/python_scripts/Interchange_app/run_interchange.sh "
+runn_command = "docker run -ti interchange-python-docker "
 if os.path.exists(runn_command):
    t1 = BashOperator(
         task_id= 'create_file',
@@ -27,3 +56,5 @@ if os.path.exists(runn_command):
    )
 else:
     raise Exception("Cannot locate {}".format(runn_command))
+    
+"""
